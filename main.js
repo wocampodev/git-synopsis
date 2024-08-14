b9b7980017250e4f5484924a623100e057024961
juan-jolo-21 <juanlorenzo_800@hotmail.com>
2024-08-14 11:52:56 -0500

    rama feature jolo

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 import { consoleGreeting } from './greeting'
-
+let empresa = 'synopsis'
 console.log('hola')
 console.log('Raquel')
 consoleGreeting('Arnold!')
 console.log("Agregando user: Adolfo");
 console.log('juan jolo')
-
+console.log(`bienvenidos a ${empresa}`)
