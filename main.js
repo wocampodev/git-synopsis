1fcbe3e5a04386aa210f5a7e3059481c83fabf21
juan-jolo-21 <juanlorenzo_800@hotmail.com>
2024-08-14 12:19:20 -0500

    new change

--- main.js
+++ main.js
@@ -8,10 +8,7 @@ consoleGreeting('Synopsis Trainee!')
 console.log('Raquel')
 consoleGreeting('Arnold!')
 console.log("Agregando user: Adolfo");
-<<<<<<< HEAD
 console.log('juan jolo 21')
 
 console.log("");
-=======
 console.log("GAAAAAAAA");
->>>>>>> origin/main
